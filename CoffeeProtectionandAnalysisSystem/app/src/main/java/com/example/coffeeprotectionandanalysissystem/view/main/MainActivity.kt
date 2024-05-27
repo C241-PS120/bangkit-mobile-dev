6e0822a22acd7c08b9298cc995ceb0f0b4c8c5ff
OnlyJamal <98729482+adhitia92@users.noreply.github.com>
2024-05-28 02:46:50 +0700

    Pembahruan
    COPTAS

---
+++ CoffeeProtectionandAnalysisSystem/app/src/main/java/com/example/coffeeprotectionandanalysissystem/view/main/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.coffeeprotectionandanalysissystem.view.main
+
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.WindowInsetsController
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.coffeeprotectionandanalysissystem.R
+import com.example.coffeeprotectionandanalysissystem.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar: Toolbar = findViewById(R.id.topbar)
+        setSupportActionBar(toolbar)
+
+        setupView()
+
+        val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+        // Konfigurasi AppBar dengan destinasi-level-atas yang sesuai
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.top_nav_menu, menu)
+        return true
+    }
+
+    private fun setupView() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.setSystemBarsAppearance(
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+            )
+            window.statusBarColor = Color.TRANSPARENT
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                            View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                    )
+            window.statusBarColor = Color.TRANSPARENT
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                // Tambahkan kode untuk menangani klik pada item overflow menu di sini
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
