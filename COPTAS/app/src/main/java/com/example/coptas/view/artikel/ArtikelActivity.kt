e41cb4abb1cb304691b72762a5bb3e76660d803f
OnlyJamal <98729482+adhitia92@users.noreply.github.com>
2024-05-28 00:44:52 +0700

    Main Activity
    Perbaikan lagi

--- COPTAS/app/src/main/java/com/example/coptas/view/artikel/ArtikelActivity.kt
+++ COPTAS/app/src/main/java/com/example/coptas/view/artikel/ArtikelActivity.kt
@@ -1,21 +1,79 @@
 package com.example.coptas.view.artikel
 
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.WindowInsetsController
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityOptionsCompat
 import com.example.coptas.R
+import com.example.coptas.view.main.MainActivity
+import com.example.coptas.view.riwayat.HistoryActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ArtikelActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_artikel)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setupView()
+
+        val toolbar: Toolbar = findViewById(R.id.topbar)
+        setSupportActionBar(toolbar)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            handleNavigation(item)
+        }
+
+        bottomNavigationView.selectedItemId = R.id.article
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    private fun handleNavigation(item: MenuItem): Boolean {
+        val intent = when (item.itemId) {
+            R.id.home -> Intent(this, MainActivity::class.java)
+            R.id.article -> {
+                return true
+            }
+            R.id.history -> Intent(this, HistoryActivity::class.java)
+            else -> return false
+        }
+        startActivity(intent)
+
+        val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this)
+        startActivity(intent, options.toBundle())
+        return true
+    }
+
+    private fun setupView() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.setSystemBarsAppearance(
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+            )
+            window.statusBarColor = Color.TRANSPARENT
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                            View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                    )
+            window.statusBarColor = Color.TRANSPARENT
         }
     }
 }
--- COPTAS/app/src/main/java/com/example/coptas/view/main/MainActivity.kt
+++ COPTAS/app/src/main/java/com/example/coptas/view/main/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.coptas.view.main
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.WindowInsetsController
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityOptionsCompat
 import com.example.coptas.R
+import com.example.coptas.view.artikel.ArtikelActivity
+import com.example.coptas.view.riwayat.HistoryActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +25,11 @@ class MainActivity : AppCompatActivity() {
 
         val toolbar: Toolbar = findViewById(R.id.topbar)
         setSupportActionBar(toolbar)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            handleNavigation(item)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -27,6 +38,19 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    private fun handleNavigation(item: MenuItem): Boolean {
+        val intent: Intent = when (item.itemId) {
+            R.id.home -> Intent(this, MainActivity::class.java)
+            R.id.article -> Intent(this, ArtikelActivity::class.java)
+            R.id.history -> Intent(this, HistoryActivity::class.java)
+            else -> return false
+        }
+        startActivity(intent)
+
+        val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this)
+        startActivity(intent, options.toBundle())
+        return true
+    }
 
     private fun setupView() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
@@ -43,9 +67,7 @@ class MainActivity : AppCompatActivity() {
                             View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
                             View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
                     )
-            @Suppress("DEPRECATION")
             window.statusBarColor = Color.TRANSPARENT
         }
     }
-
 }
