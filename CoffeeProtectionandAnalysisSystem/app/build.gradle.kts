3f6801136755c0d385a3a6b9cb11f8665b72d84a
irgydaffa <irgytl12@gmail.com>
2024-06-15 21:52:06 +0700

    minor fix

--- CoffeeProtectionandAnalysisSystem/app/build.gradle.kts
+++ CoffeeProtectionandAnalysisSystem/app/build.gradle.kts
@@ -67,10 +67,12 @@ dependencies {
     implementation("com.android.car.ui:car-ui-lib:2.6.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation("androidx.activity:activity:1.9.0")
+    implementation("androidx.test:rules:1.5.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation ("com.google.truth:truth:1.1.3")
 
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.datastore:datastore-core:1.1.1")
