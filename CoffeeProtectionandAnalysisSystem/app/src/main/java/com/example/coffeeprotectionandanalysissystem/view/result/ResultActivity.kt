f18dedd44f06b4e79c2f4363d6b83c4e8429f7ea
irgydaffa <irgytl12@gmail.com>
2024-06-18 00:46:46 +0700

    improving detailhistory UI

--- CoffeeProtectionandAnalysisSystem/app/src/main/java/com/example/coffeeprotectionandanalysissystem/view/result/ResultActivity.kt
+++ CoffeeProtectionandAnalysisSystem/app/src/main/java/com/example/coffeeprotectionandanalysissystem/view/result/ResultActivity.kt
@@ -52,6 +52,13 @@ class ResultActivity : AppCompatActivity() {
             val currentDate = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(Date())
             saveToDatabase(imageUrl, label, suggestion, currentDate, symptoms)
         }
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        setupView()
     }
 
     private fun setupRecyclerView() {
@@ -107,5 +114,24 @@ class ResultActivity : AppCompatActivity() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    private fun setupView() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.setSystemBarsAppearance(
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+            )
+            window.statusBarColor = Color.TRANSPARENT
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                            View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                    )
+            window.statusBarColor = Color.TRANSPARENT
+        }
     }
 }
